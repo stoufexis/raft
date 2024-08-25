eb465e8a141c972648f3f4aba0045d49cb652f06
Stefanos Toufexis <stefanos.toufexis@noris.gr>
2024-08-25 19:25:46 +0300

    savepoint

--- src/main/scala/com/stoufexis/leader/statemachine/Leader.scala
+++ src/main/scala/com/stoufexis/leader/statemachine/Leader.scala
@@ -21,20 +21,27 @@ import scala.concurrent.duration.FiniteDuration
 object Leader:
   case class CommittableIndex[F[_]](index: Index, commit: F[Unit])
 
-  class LocalLog[F[_]: Monad, A](
-    log:              Log[F, A],
-    uncommittedQueue: Queue[F, (Index, Map[NodeId, Deferred[F, Unit]])]
+  class CommitLog[F[_]: Monad, A](
+    log:         Log[F, A],
+    uncommitted: SignallingRef[F, Index],
+    matchIdxs:   SignallingRef[F, Map[NodeId, Index]]
   ):
     /** Wait until a majority commits the entry
       */
     def appendAndWait(nodes: Set[NodeId], term: Term, entry: A): F[Unit] =
       ???
 
-    def sendInfo(beginAt: Index, endAt: Index): F[(Term, Index, Chunk[A])] = ???
+    def getMatchIndex(node: NodeId): F[Option[Index]] =
+      ???
+
+    def setMatchIndex(node: NodeId, idx: Index): F[Unit] =
+      ???
+
+    def sendInfo(beginAt: Index): F[(Term, Index, Chunk[A])] = ???
 
     /** If there is no new index for advertiseEvery duration, repeat the previous index
       */
-    def uncommitted(node: NodeId, advertiseEvery: FiniteDuration): Stream[F, CommittableIndex[F]] =
+    def uncommitted(node: NodeId, advertiseEvery: FiniteDuration): Stream[F, Index] =
       ???
 
   def apply[F[_]: Logger, A, S](
@@ -71,17 +78,15 @@ object Leader:
         case IncomingVote(req, sink) =>
           req.grant(sink) as Some(state.transition(Role.VotedFollower, req.term))
 
-    def appender(localLog: LocalLog[F, A]): List[Stream[F, NodeInfo[S]]] =
-      def send(
-        matchIdx:    Index,
-        node:        NodeId,
-        committable: CommittableIndex[F],
-        seek:        Boolean = false
-      ): F[Either[NodeInfo[S], Index]] =
+    def appender(localLog: CommitLog[F, A]): List[Stream[F, NodeInfo[S]]] =
+      def send(matchIdx: Index, node: NodeId, seek: Boolean = false): F[Either[NodeInfo[S], Index]] =
         val response: F[(AppendResponse, Index)] =
           for
-            (matchIdxTerm, commitIdx, entries) <-
-              localLog.sendInfo(matchIdx, committable.index)
+            (matchIdxTerm, commitIdx, as) <-
+              localLog.sendInfo(matchIdx)
+
+            entries: Chunk[A] = 
+              if seek then Chunk.empty else as
 
             request: AppendEntries[A] =
               AppendEntries(
@@ -90,7 +95,7 @@ object Leader:
                 prevLogIndex = matchIdx,
                 prevLogTerm  = matchIdxTerm,
                 leaderCommit = commitIdx,
-                entries      = if seek then Chunk.empty else entries
+                entries      = entries
               )
 
             response: AppendResponse <-
@@ -99,50 +104,57 @@ object Leader:
 
         response.flatMap:
           case (AppendResponse.Accepted, newMatchIdx) if seek =>
-            send(matchIdx, node, committable, seek = false)
+            send(matchIdx, node, seek = false)
           case (AppendResponse.Accepted, newMatchIdx) =>
-            committable.commit as (Right(newMatchIdx))
+            F.pure(Right(newMatchIdx))
           case (AppendResponse.NotConsistent, _) =>
-            send(matchIdx.previous, node, committable, seek = true)
+            send(matchIdx.previous, node, seek = true)
           case (AppendResponse.TermExpired(newTerm), _) =>
             F.pure(Left(state.transition(Role.Follower, newTerm)))
           case (AppendResponse.IllegalState(state), _) =>
             F.raiseError(IllegalStateException(state))
 
-      /** Maintains a matchIndex for each node, which points to the last index in the log for which
-        * every preceeding entry matches, including the matchIndex entry.
+      /** Maintains a matchIndex for each node, which points to the last index in the log for which every
+        * preceeding entry matches, including the matchIndex entry.
         *
-        * If there is a new uncommitted index, it attempts to send the uncommitted records to the
-        * node. If the node returns a NotConsistent, then attempt to find the largest index for
-        * which the logs match, by decreasing the matchIndex by 1 and attempting to send an empty
-        * AppendEntries.
+        * If there is a new uncommitted index, it attempts to send the uncommitted records to the node.
+        * If the node returns a NotConsistent, then attempt to find the largest index for which the logs
+        * match, by decreasing the matchIndex by 1 and attempting to send an empty AppendEntries.
         *
-        * If there is no new uncommitted index and heartbeatEvery time has passed, will emit
-        * heartbeat, which is a seek request, ie an append entries with no entries. The heartbeat
-        * may also fail the consistency check, in which case a seek is attempted to find the correct
-        * matchIndex, and then a send is attempted.
+        * If there is no new uncommitted index and heartbeatEvery time has passed, will emit heartbeat,
+        * which is a seek request, ie an append entries with no entries. The heartbeat may also fail the
+        * consistency check, in which case a seek is attempted to find the correct matchIndex, and then a
+        * send is attempted.
         *
-        * If we encounter a TermExpired, emit a new state, which signals the upstream for this
-        * stream's termination. In all other cases nothing is emitted.
+        * If we encounter a TermExpired, emit a new state, which signals the upstream for this stream's
+        * termination. In all other cases nothing is emitted.
         */
       def forNode(node: NodeId): Stream[F, NodeInfo[S]] =
         localLog
           .uncommitted(node, cfg.heartbeatEvery)
-          .evalMapFilterAccumulate(Option.empty[Index]): (matchIdx, nextIdx) =>
-            send(matchIdx.getOrElse(nextIdx), node).map:
-              case Left(err)          => (matchIdx, Some(err))
-              case Right(newMatchIdx) => (Some(newMatchIdx), None)
+          .evalMapFilter: nextIdx =>
+            for
+              matchIdx: Option[Index] <-
+                localLog.getMatchIndex(node)
+
+              result: Either[NodeInfo[S], Index] <-
+                send(matchIdx.getOrElse(nextIdx), node)
+
+              out: Option[NodeInfo[S]] <- result match
+                case Left(newState)     => F.pure(Some(newState))
+                case Right(newMatchIdx) => localLog.setMatchIndex(node, newMatchIdx) as None
+            yield out
 
       state.otherNodes.map(forNode).toList
     end appender
 
-    def handleAppends(localLog: LocalLog[F, A]): Stream[F, Nothing] =
+    def handleAppends(localLog: CommitLog[F, A]): Stream[F, Nothing] =
       Stream
         .fromQueueUnterminated(appends)
         .evalScan(state.automatonState):
           case (acc, (newEntry, sink)) =>
             for
-              _    <- localLog.appendAndWait(state.term, newEntry)
+              // _    <- localLog.appendAndWait(state.term, newEntry)
               newS <- automaton(acc, newEntry)
               _    <- sink.complete_(newS)
             yield newS
