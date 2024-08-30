cddb9f0d5702197ecc178a73439d5ff88cb66576
Stefanos Toufexis <stefanos.toufexis@noris.gr>
2024-08-30 22:19:54 +0300

    Better RequestQueue implementation

--- src/main/scala/com/stoufexis/leader/rpc/RequestQueue.scala
+++ src/main/scala/com/stoufexis/leader/rpc/RequestQueue.scala
@@ -1,58 +1,84 @@
 package com.stoufexis.leader.rpc
 
-import cats.effect.implicits.given
 import cats.effect.kernel.*
 import cats.effect.std.*
 import cats.implicits.given
 import fs2.*
-import fs2.concurrent.SignallingRef
 
 import scala.collection.immutable.SortedMap
 
 /** A request is committed, and thus dequeued, only if a response is produced. Any later consumers will
   * see the same request. Used to implement RPC.
-  * 
+  *
   * TODO: Test
   */
 trait RequestQueue[F[_], I, O]:
   def offer(input: I): F[O]
 
+  /** Someone needs to always be consuming, otherwise offer requests will hang forever
+    */
   def consume: Stream[F, (I, Deferred[F, O])]
 
 object RequestQueue:
-  def bounded[F[_], I, O](bound: Int)(using
-    F: Concurrent[F]
-  ): F[RequestQueue[F, I, O]] =
+  def bounded[F[_], I, O](bound: Int)(using F: Concurrent[F]): Resource[F, RequestQueue[F, I, O]] =
+
+    def raceMap(head: (Int, (I, Deferred[F, O])), map: Map[Int, (I, Deferred[F, O])]): F[Int] =
+      ???
+
     for
-      map: SignallingRef[F, (Int, SortedMap[Int, (I, Deferred[F, O])])] <-
-        SignallingRef[F].of(0, SortedMap.empty[Int, (I, Deferred[F, O])])
+      inputs: Queue[F, (I, Deferred[F, O])] <-
+        Resource.eval(Queue.synchronous)
 
-      sem: Semaphore[F] <-
-        Semaphore[F](bound)
+      outputQueue: Queue[F, Queue[F, (I, Deferred[F, O])]] <-
+        Resource.eval(Queue.synchronous)
+
+      sup: Supervisor[F] <-
+        Supervisor[F](await = false)
+
+      background: F[Unit] =
+        def go(
+          waiting: SortedMap[Int, (I, Deferred[F, O])],
+          output:  Queue[F, (I, Deferred[F, O])],
+          idx:     Int
+        ): F[Unit] =
+          val inputOutputQueue: F[Either[(I, Deferred[F, O]), Queue[F, (I, Deferred[F, O])]]] =
+            if waiting.size <= bound then
+              F.race(inputs.take, outputQueue.take)
+            else // Dont attempt to take from input if bound is reached
+              outputQueue.take.map(Right(_))
+
+          val raced: F[Either[Int, Either[(I, Deferred[F, O]), Queue[F, (I, Deferred[F, O])]]]] =
+            waiting.headOption match
+              case None =>
+                inputOutputQueue.map(Right(_))
+
+              case Some(head) =>
+                F.race(raceMap(head, waiting.tail), inputOutputQueue)
+
+          raced.flatMap:
+            // A deferred completed
+            case Left(i) =>
+              go(waiting.removed(i), output, idx)
+
+            // New input
+            case Right(Left(inp)) =>
+              output.offer(inp)
+                >> go(waiting.updated(idx + 1, inp), output, idx + 1)
+
+            // Consumer change
+            case Right(Right(newOutput)) =>
+              waiting.values.toList.traverse(newOutput.offer)
+                >> go(waiting, newOutput, idx)
+
+        outputQueue.take.flatMap(go(SortedMap.empty, _, 0))
+
+      _ <-
+        Resource.eval(sup.supervise(background))
     yield new RequestQueue[F, I, O]:
-      def consume: Stream[F, (I, Deferred[F, O])] =
-        map
-          .discrete
-          .map(_._2)
-          .mapAccumulate(Set.empty[Int]): (prev, next) =>
-            (next.keySet, next.filterNot(x => prev.contains(x._1)))
-          .flatMap: (_, newElems) =>
-            Stream.iterable(newElems.values)
-
-      def set(input: I, deferred: Deferred[F, O]): F[Int] =
-        map.modify: (i, map) =>
-          val i2: Int =
-            if i == Int.MaxValue then 0 else i + 1
-
-          val updated: SortedMap[Int, (I, Deferred[F, O])] =
-            map.updated(i2, (input, deferred))
-
-          ((i2, updated), i2)
-
-      def unset(key: Int): F[Unit] =
-        map.update((i, m) => (i, m.removed(key)))
-
       def offer(input: I): F[O] =
-        sem.permit.surround:
-          F.deferred[O].flatMap: deferred =>
-            set(input, deferred).flatMap(k => deferred.get.guarantee(unset(k)))
+        F.deferred[O].flatMap: d =>
+          inputs.offer(input, d) >> d.get
+
+      def consume: Stream[F, (I, Deferred[F, O])] =
+        Stream.eval(Queue.unbounded[F, (I, Deferred[F, O])]).flatMap: q =>
+          Stream.eval(outputQueue.offer(q)) >> Stream.fromQueueUnterminated(q)
