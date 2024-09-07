9fd65e4d22b8021c279449fe24717e8a7f3a04b3
Stefanos Toufexis <stefanos.toufexis@noris.gr>
2024-09-07 19:43:28 +0300

    Apply NamedLogger

--- modules/raft/src/main/scala/com/stoufexis/raft/service/NamedLogger.scala
+++ modules/raft/src/main/scala/com/stoufexis/raft/service/NamedLogger.scala
@@ -1,10 +1,18 @@
 package com.stoufexis.raft.service
 
-import com.stoufexis.raft.model.NodeInfo
+import cats.effect.kernel.Sync
 import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+
+import com.stoufexis.raft.model.NodeInfo
+import cats.implicits.given
 
 trait NamedLogger[F[_]]:
   def fromState(state: NodeInfo[?]): F[Logger[F]]
 
 object NamedLogger:
   def apply[F[_]: NamedLogger]: NamedLogger[F] = summon
+
+  given [F[_]: Sync]: NamedLogger[F] with
+    def fromState(state: NodeInfo[?]): F[Logger[F]] =
+      Slf4jFactory.create[F].fromName(state.print).widen
--- modules/raft/src/main/scala/com/stoufexis/raft/statemachine/StateMachine.scala
+++ modules/raft/src/main/scala/com/stoufexis/raft/statemachine/StateMachine.scala
@@ -9,6 +9,7 @@ import org.typelevel.log4cats.Logger
 
 import com.stoufexis.raft.model.*
 import com.stoufexis.raft.rpc.*
+import com.stoufexis.raft.service.NamedLogger
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -18,19 +19,23 @@ object StateMachine:
     heartbeatEvery: FiniteDuration,
     automaton:      (S, A) => S
   )(using
-    Temporal[F],
+    Async[F],
     Monoid[S],
-    Logger[F],
     RPC[F, A, S],
     Log[F, A],
     Timeout[F]
   ): F[Nothing] =
     def go(st: NodeInfo[S], chan: Channel[F, NodeInfo[S]]): Stream[F, Nothing] =
       val behaviors: Resource[F, Behaviors[F, S]] =
-        st.role match
-          case Role.Follower  => Resource.eval(Follower(st))
-          case Role.Candidate => Resource.eval(Candidate(st))
-          case Role.Leader    => Leader(st, heartbeatEvery, automaton)
+        for
+          given Logger[F] <-
+            Resource.eval(NamedLogger[F].fromState(st))
+
+          behaviors <- st.role match
+            case Role.Follower  => Resource.eval(Follower(st))
+            case Role.Candidate => Resource.eval(Candidate(st))
+            case Role.Leader    => Leader(st, heartbeatEvery, automaton)
+        yield behaviors
 
       // Works like parJoinUnbounded, but reuses the same channel and only ever outputs 1 element
       val joined: Stream[F, NodeInfo[S]] =
