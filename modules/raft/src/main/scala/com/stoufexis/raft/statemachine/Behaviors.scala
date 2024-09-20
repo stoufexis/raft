ce60d5b51cc9c5dab327fedc7513330b0ba34ab9
Stefanos Toufexis <stefanos.toufexis@noris.gr>
2024-09-20 23:02:10 +0300

    Safer behaviors runner

--- modules/raft/src/main/scala/com/stoufexis/raft/statemachine/Behaviors.scala
+++ modules/raft/src/main/scala/com/stoufexis/raft/statemachine/Behaviors.scala
@@ -1,11 +1,17 @@
 package com.stoufexis.raft.statemachine
 
+import cats.effect.implicits.given
 import cats.effect.kernel.*
+import cats.implicits.given
 import fs2.*
 
 case class Behaviors[F[_]](streams: List[Stream[F, NodeInfo]]):
-  def raceFirst(using Concurrent[F]): F[NodeInfo] =
-    streams.map(_.head).parJoinUnbounded.head.compile.lastOrError
+  def raceFirst(using F: Concurrent[F]): F[NodeInfo] =
+    for
+      ni     <- Deferred[F, NodeInfo]
+      joined <- F.pure(streams.parTraverse_(_.head.evalTap(ni.complete).compile.drain))
+      out    <- F.race(joined >> F.never[Nothing], ni.get)
+    yield out.merge
 
   infix def ++(others: Seq[Stream[F, NodeInfo]]): Behaviors[F] =
     Behaviors(streams ++ others)
